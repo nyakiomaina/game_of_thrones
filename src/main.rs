2230b35ed257e102baebe6e1cfdcbe67870f7897
nyakiomaina <carolinenyakio21@gmail.com>
2023-11-16 12:08:31 +0300

    westros conquest

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,112 @@
-struct Kingdom {
-    name: String,
-    ruler: String,
+use clap::{Arg, Command};
+use rand::Rng;
+use serde::{Serialize, Deserialize};
+use serde_json;
+use std::collections::HashMap;
+use std::fs;
+use std::path::Path;
+
+#[derive(Serialize, Deserialize)]
+struct Game {
+    kingdoms: HashMap<String, String>,
+}
+
+impl Game {
+    fn new() -> Self {
+        Self {
+            kingdoms: HashMap::new(),
+        }
+    }
+
+    fn add_kingdom(&mut self, name: &str, ruler: &str) {
+        self.kingdoms.insert(name.to_string(), ruler.to_string());
+    }
+
+    fn list_kingdoms(&self) {
+        for (kingdom, ruler) in &self.kingdoms {
+            println!("{}: Ruled by {}", kingdom, ruler);
+        }
+    }
+
+    fn battle(&mut self, kingdom: &str, challenger: &str) {
+        let rng = rand::thread_rng().gen_range(0..=1);
+        let current_ruler = self.kingdoms.get(kingdom);
+        match current_ruler {
+            Some(ruler) => {
+                if rng == 1 {
+                    println!("{} has overthrown {} in {}!", challenger, ruler, kingdom);
+                    self.kingdoms.insert(kingdom.to_string(), challenger.to_string());
+                } else {
+                    println!("{} failed to conquer {} and {} retains control!", challenger, kingdom, ruler);
+                }
+            },
+            None => println!("The kingdom of {} does not exist.", kingdom),
+        }
+    }
+
+    fn save(&self) {
+        let json = serde_json::to_string(&self).expect("Error serializing game state.");
+        fs::write("game_state.json", json).expect("Error saving game state.");
+    }
+
+    fn load() -> Self {
+        if Path::new("game_state.json").exists() {
+            let json = fs::read_to_string("game_state.json").expect("Error loading game state.");
+            serde_json::from_str(&json).expect("Error deserializing game state.")
+        } else {
+            Self::new()
+        }
+    }
 }
 
 fn main() {
-    let kingdoms = vec![
-        Kingdom {
-            name: "The North".to_string(),
-            ruler: "House Stark".to_string(),
-        },
-        Kingdom {
-            name: "The Vale".to_string(),
-            ruler: "House Arryn".to_string(),
-        },
-        Kingdom {
-            name: "The Riverlands".to_string(),
-            ruler: "House Tully".to_string(),
-        },
-        Kingdom {
-            name: "The Westerlands".to_string(),
-            ruler: "House Lannister".to_string(),
-        },
-        Kingdom {
-            name: "The Iron Islands".to_string(),
-            ruler: "House Greyjoy".to_string(),
-        },
-        Kingdom {
-            name: "The Reach".to_string(),
-            ruler: "House Tyrell".to_string(),
-        },
-        Kingdom {
-            name: "The Stormlands".to_string(),
-            ruler: "House Baratheon".to_string(),
-        },
-        Kingdom {
-            name: "Dorne".to_string(),
-            ruler: "House Martell".to_string(),
-        },
-        Kingdom {
-            name: "The Crownlands".to_string(),
-            ruler: "House Baratheon of King's Landing".to_string(),
-        },
-    ];
+    let matches = Command::new("Westeros Conquest")
+        .version("1.0")
+        .author("Nyakio")
+        .about("Simulates conquests in the Game of Thrones universe")
+        .subcommand(
+            Command::new("list")
+                .about("Lists all kingdoms and their rulers"),
+        )
+        .subcommand(
+            Command::new("add")
+                .about("Adds a new kingdom and its ruler")
+                .arg(Arg::new("kingdom").help("The name of the kingdom").required(true))
+                .arg(Arg::new("ruler").help("The ruler of the kingdom").required(true)),
+        )
+        .subcommand(
+            Command::new("battle")
+                .about("Challenge the ruler of a kingdom")
+                .arg(Arg::new("kingdom").help("The name of the kingdom to challenge").required(true))
+                .arg(Arg::new("challenger").help("The name of the challenger").required(true)),
+        )
+        .get_matches();
 
-    for kingdom in kingdoms {
-        println!("The Kingdom of {}: Ruled by {}", kingdom.name, kingdom.ruler);
+    let mut game = Game::load();
+
+    match matches.subcommand() {
+        Some(("list", _)) => {
+            game.list_kingdoms();
+        },
+        Some(("add", add_matches)) => {
+            let kingdom_name = add_matches.get_one::<String>("kingdom").expect("required");
+            let ruler_name = add_matches.get_one::<String>("ruler").expect("required");
+            game.add_kingdom(kingdom_name, ruler_name);
+            println!("Added kingdom: {} ruled by {}", kingdom_name, ruler_name);
+        },
+        Some(("battle", battle_matches)) => {
+            let kingdom_name = battle_matches.get_one::<String>("kingdom").expect("required");
+            let challenger_name = battle_matches.get_one::<String>("challenger").expect("required");
+            game.battle(kingdom_name, challenger_name);
+        },
+        _ => {
+            println!("Westeros Conquest");
+            println!("Use the following commands to play:");
+            println!("list - Lists all kingdoms and their rulers-- make sure you have added the kingdoms and rulers first");
+            println!("add <kingdom> <ruler> - Adds a new kingdom with its ruler");
+            println!("battle <kingdom> <challenger> - Challenge the current ruler of a kingdom");
+        },
     }
+
+    game.save();
 }
